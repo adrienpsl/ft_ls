074e1efdf500d93d5d6776cff622a828fc8ebd2b
adpusel <adrien.pusel@gmail.com>
2019-07-04 12:55:36 +0200

    ce putain du srcipt marche enfin en etant bien simplifier c'est vraiment un enfer le shell

--- test.sh
+++ test.sh
@@ -9,35 +9,40 @@
 #    Script Option
 # *------------------------------------*/
 declare showDiff=1;
-declare showOk=0;
+declare showOk=1;
 declare showTitle=0;
 
 # *------------------------------------*\
 #    Variables use in that script
 # *------------------------------------*/
 declare     TITLE;
-declare -a  ARRAY;
-declare -a  COMMANDS;
-declare     OPTIONS;
-declare     FILES;
+declare     COMMANDS;
+declare     COMMAND;
 
 declare     ERROR;
 declare     DIFF;
 
-declare     PATH_TEST_DIR="../cmp_dir";
-declare     PATH_WORKING="working_dir"
-declare     OUR="${PATH_TEST_DIR}/our";
-declare     YOUR="${PATH_TEST_DIR}/your";
+declare     PATH_DIR="ft-ls-test";
+declare     PATH_LOG="ft-ls-log";
 
-# une options pour activer ou non les diffs pcq ca prends vite de la place cette merde !
-# faire des titres en petit, ca sert a rien de faire le ouf avec des trucs de malades !
-# voila voila ! :).
-# demander sur le forum ce que pense les gens sur l'algo de trie de ls
+declare     LS_LOG="${PATH_LOG}/our";
+declare     YOUR_LOG="${PATH_LOG}/your";
 
-# faire un test de segfault
-# faire un test de vitesse
-# prendre les test de truc et les applique a mon projet
-# rester simple c'est la clee, le 42filesheker est overkill je trouve, c'est un peu trop lours pour moi
+function resetWorkingDir()
+{
+    # je reset le directory
+    chmod   -R 777 ${PATH_DIR} && rm -rf ${PATH_DIR} && mkdir ${PATH_DIR}
+    # je met ls dedans
+    make  > /dev/null && cp ./ft_ls ${PATH_DIR}
+    echo
+}
+
+# *------------------------------------*\
+#    build and create program
+# *------------------------------------*/
+# je reset les logs
+rm -rf  ${PATH_LOG} && mkdir ${PATH_LOG}
+resetWorkingDir
 
 # *------------------------------------*\
 #    Functions
@@ -45,23 +50,26 @@ declare     YOUR="${PATH_TEST_DIR}/your";
 
 function testLs()
 {
-    ERROR=$( {  /bin/ls ${OPTIONS} ${FILES}  > ${OUR} \
-                && ${PATH_WORKING}/ft_ls ${OPTIONS} ${FILES} > ${YOUR} ; } 2>&1 )
-    # brief test file, without print the diff
-    DIFF=$(  { diff --brief ${OUR} ${YOUR} ; } 2>&1 )
-}
+    # make set up
+    cd ${PATH_DIR} ; ${SETUP}
 
-function printRes()
-{
+    # do test
+    ERROR=$( {  /bin/ls              ${COMMAND}  > ../${LS_LOG} \
+                && ./ft_ls ${COMMAND}  > ../${YOUR_LOG} ; } \
+                2>&1 )
+
+    cd ..;
+
+    DIFF=$(  { diff --brief ${LS_LOG} ${YOUR_LOG} ; } 2>&1 )
+
     if [[ -n ${ERROR} ]]; then
-        echo  "\033[31mNo test the current error append:\033[37;1m \n$ERROR ---------- \033[0m"
+        echo  "\033[31mNo test the current error append:\033[37;1m $ERROR \033[0m"
     elif [[ -n ${DIFF} ]]; then
-        # tee write the data in the file ( append mode ), and show there in the console, \
-        # 1i / 2o like -|- a T connector :)
-        echo "\r\033[31mTest ko  : ${OPTIONS} ${FILES}\033[0m"  | tee -a err-log-ls.txt;
-	   [[ ${showDiff} -gt 0 ]]  && diff -y ${OUR} ${YOUR} | colordiff >> err-log-ls.txt; # you need to install that with brew ( in mac os )
-	else
-	   [[ ${showOk} -gt 0 ]] && echo "\033[32mTest ok  : ${OPTIONS} ${FILES} \033[0m"
+        echo "\r\033[31mTest ko  : ${COMMAND}\033[0m"  | tee -a ${PATH_LOG}/err-log.txt;
+        [[ ${showDiff} -gt 0 ]]  && diff -y ${LS_LOG} ${YOUR_LOG} | colordiff >> ${PATH_LOG}/err-log-ls.txt; # you need to install that with brew ( in mac os )
+        diff -y ${LS_LOG} ${YOUR_LOG} | colordiff
+    else
+        [[ ${showOk} -gt 0 ]] && echo "\033[32mTest ok  : ${COMMAND} \033[0m"
     fi
 }
 
@@ -74,41 +82,19 @@ function printTitle()
 
 function parseAndDo()
 {
-     [[ ${showTitle} -gt 0 ]] && printTitle;
+    [[ ${showTitle} -gt 0 ]] && printTitle;
 
-    for command in "${COMMANDS[@]}"
-    do
-        # split the line and reset the original IFS
-        ORIGINAL_IFS=${IFS}
-        IFS='~'
-        read -r -a argv <<< ${command};
-        IFS=${ORIGINAL_IFS}
+    COMMAND=$1
+    SETUP=$2
 
-        OPTIONS=${argv[0]}
-        FILES=${argv[1]}
-
-        testLs;
-        printRes;
-    done
+#    echo "$COMMAND $SETUP"
+    testLs;
     echo
 }
 
 
 # if -l, the symlink is treat like a file.
 
-# *------------------------------------*\
-#    build and create program
-# *------------------------------------*/
-chmod   -R 777          ${PATH_WORKING}  > /dev/null
-rm      -rf             ${PATH_WORKING}
-mkdir                   ${PATH_WORKING}
-make                                      > /dev/null
-mv ./ft_ls              ${PATH_WORKING}
-
-rm      -rf             ${PATH_TEST_DIR}  > /dev/null
-mkdir                   ${PATH_TEST_DIR}  > /dev/null
-rm -f  err-log-ls.txt
-echo
 
 # faire le check de nv.
 # faire le test de l'usage.
@@ -118,105 +104,5 @@ echo
 #    test Error Handling
 # *------------------------------------*/
 TITLE="Error Handling"
-COMMANDS=(
-    "-l ~ /Users/adpusel  ~ " # ici je fais un truc pour set up l'env de test ?
-    "-l ~ . ."
-    "-l ~   "
-    "-l ~ /"
-)
-parseAndDo
 
-
-
-
-
-
-## *------------------------------------*\
-##   test with no col
-## *------------------------------------*/
-#./ft_ls__run -1 /Users/adpusel          > me.txt
-#ls -1 /Users/adpusel                    > ls.txt
-#diff me.txt ls.txt
-#
-## *------------------------------------*\
-##    test with long format
-## *------------------------------------*/
-#./ft_ls__run -l /Users/adpusel          > me.txt
-#ls -l /Users/adpusel                    > ls.txt
-#diff me.txt ls.txt
-
-## *------------------------------------*\
-##    test with -R
-## *------------------------------------*/
-#time ./ft_ls__run -1R /Users/adpusel          > me.txt
-#time ls -1R /Users/adpusel                    > ls.txt
-#diff me.txt ls.txt
-
-
-## *------------------------------------*\
-##    test with -test
-## *------------------------------------*/
-#./ft_ls__run -l /Users/adpusel/Downloads          > me.txt
-#ls -l /Users/adpusel/Downloads                    > ls.txt
-#diff me.txt ls.txt
-
-## *------------------------------------*\
-##    test with /dev driver
-## *------------------------------------*/
-#./ft_ls__run -l /dev         > me.txt
-#ls -l /dev                    > ls.txt
-#diff me.txt ls.txt
-
-
-## *------------------------------------*\
-##    test with /var/run socket
-## *------------------------------------*/
-#./ft_ls__run -l /var/run          > me.txt
-#ls -l /var/run                    > ls.txt
-#diff me.txt ls.txt
-
-
-## *------------------------------------*\
-##    test with -t
-## *------------------------------------*/
-#./ft_ls__run -lt /Users/adpusel/Downloads          > me.txt
-#ls -lt /Users/adpusel/Downloads                    > ls.txt
-#diff me.txt ls.txt
-#
-#
-## *------------------------------------*\
-##    test with -rt
-## *------------------------------------*/
-#./ft_ls__run -lrt /Users/adpusel/Downloads          > me.txt
-#ls -lrt /Users/adpusel/Downloads                    > ls.txt
-#diff me.txt ls.txt
-
-# *------------------------------------*\
-#    test with -S
-#    here to diff is same size between file.
-# *------------------------------------*/
-#./ft_ls__run -lS /Users/adpusel/Downloads           > me.txt
-#ls -lS /Users/adpusel/Downloads                     > ls.txt
-#diff me.txt ls.txt
-#
-#./ft_ls__run -lrS /Users/adpusel/Downloads           > me.txt
-#ls -lrS /Users/adpusel/Downloads                     > ls.txt
-#diff me.txt ls.txt
-#
-#./ft_ls__run -1rS /Users/adpusel/Downloads           > me.txt
-#ls -1rS /Users/adpusel/Downloads                     > ls.txt
-#diff me.txt ls.txt
-
-#./ft_ls__run -1S /Users/adpusel/Downloads           > me.txt
-#ls -1S /Users/adpusel/Downloads                     > ls.txt
-#diff me.txt ls.txt
-
-
-# *------------------------------------*\
-#    test_file_type
-# *------------------------------------*/
-
-#mkdir directory
-#ln -s directory dir_link
-#touch file
-
+parseAndDo  "-l . ." "mkdir oo"
